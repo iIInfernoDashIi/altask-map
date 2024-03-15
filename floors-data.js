38d728128ddf6f7e75e866954dcc265cef201310
iIInfernoDashIi <trololochkabyminecraft@gmail.com>
2024-03-15 17:35:24 +0700

    Первые наработки
    Можно двигать карту мышкой, приближать колёсиком и менять этажи стрелками вверх и вниз или шифт колёсиком

---
+++ floors-data.js
@@ -0,0 +1,56 @@
+// const f1 = `
+// <svg id="f1" class="floor" width="800" height="600" xmlns="http://www.w3.org/2000/svg">
+//     <image id="bg" href="images/f-1.png"></image>
+//     <g id="r101" class="room" tabindex="0">
+//         <polygon class="st0" points="10,10 10,110 110,110 110,10"></polygon>
+//         <text x="60" y="60">101</text>
+//     </g>
+//     <g id="r102" class="room" tabindex="0">
+//         <polygon class="st0" points="110,110 110,210 210,210 210,110"></polygon>
+//         <text x="160" y="160">102</text>
+//     </g>
+// </svg>
+// `
+
+// const f2 = `
+// <svg id="f2" class="floor" width="800" height="600" xmlns="http://www.w3.org/2000/svg">
+//     <image id="bg" href="images/f-1.png"></image>
+//     <g id="r201" class="room">
+//         <polygon class="st0" points="10,10 10,110 110,110 110,10"></polygon>
+//         <text x="60" y="60">201</text>
+//     </g>
+//     <g id="r202" class="room">
+//         <polygon class="st0" points="110,110 110,210 210,210 210,110"></polygon>
+//         <text x="160" y="160">202</text>
+//     </g>
+// </svg>
+// `
+
+const floors = {
+    f1: `
+    <svg id="f1" class="floor" width="800" height="600" xmlns="http://www.w3.org/2000/svg">
+        <image id="bg" href="images/f-1.png"></image>
+        <g id="r101" class="room" tabindex="0">
+            <polygon class="st0" points="10,10 10,110 110,110 110,10"></polygon>
+            <text x="60" y="60">101</text>
+        </g>
+        <g id="r102" class="room" tabindex="0">
+            <polygon class="st0" points="110,110 110,210 210,210 210,110"></polygon>
+            <text x="160" y="160">102</text>
+        </g>
+    </svg>
+    `,
+    f2: `
+    <svg id="f2" class="floor" width="800" height="600" xmlns="http://www.w3.org/2000/svg">
+        <image id="bg" href="images/f-1.png"></image>
+        <g id="r201" class="room">
+            <polygon class="st0" points="10,10 10,110 110,110 110,10"></polygon>
+            <text x="60" y="60">201</text>
+        </g>
+        <g id="r202" class="room">
+            <polygon class="st0" points="110,110 110,210 210,210 210,110"></polygon>
+            <text x="160" y="160">202</text>
+        </g>
+    </svg>
+    `
+}
---
+++ script.js
@@ -0,0 +1,163 @@
+/* = Мусорка =========================================== *\
+-[ ] Ивенты вынести отдельно ай гуесс
+\* ===================================================== */
+
+let map_width = 800;
+let map_height = 600;
+
+data = {
+    "r101": {
+        "name": "Sus"
+    },
+    "r102": {
+        "name": "Amogus"
+    }
+}
+
+
+
+/* = Перемещение карты ================================= *\
+\* ===================================================== */
+
+$( function() {
+    $('#map').draggable(
+        { scroll: false }
+    )
+});
+
+/* = Масштабирование карты ============================= *\
+\* ===================================================== */
+
+const min_zoom = 1;
+const max_zoom = 2.5;
+const step_zoom = 0.25;
+let current_zoom = 1;
+
+let scroll_timeout = false;
+
+const canvas = document.getElementById('canvas');
+const map = document.getElementById('map');
+canvas.addEventListener('wheel', event => {
+    if (scroll_timeout == true) { return; }
+    
+    let direction = event.deltaY > 0 ? -1 : 1;
+
+    if (event.shiftKey == true) {
+        if (direction > 0) { upFloor(); }
+        if (direction < 0) { downFloor(); }
+        scroll_timeout = true;
+        setTimeout(() => { scroll_timeout = false; }, 200);
+        return;
+    }
+
+    let new_zoom = current_zoom + direction * step_zoom;
+
+    if (new_zoom < min_zoom) { zoom(min_zoom); return; }
+    if (new_zoom > max_zoom) { zoom(max_zoom); return; }
+    zoom(new_zoom);
+});
+
+function zoom(new_zoom) {
+    current_zoom = new_zoom;
+    map.style.transform = 'scale('+current_zoom+')';
+}
+
+/* = Выбор комнаты ===================================== *\
+\* ===================================================== */
+
+let selected = '';
+
+const delta = 6;
+let start_x;
+let start_y;
+
+document.body.addEventListener('mousedown', event => {
+    start_x = event.pageX;
+    start_y = event.pageY;
+})
+
+const bg = document.getElementById('bg')
+bg.addEventListener('mouseup', event => {
+    let diffX = Math.abs(event.pageX - start_x);
+    let diffY = Math.abs(event.pageY - start_y);
+    if (diffX < delta && diffY < delta) {
+        deselect();
+        console.log("- deselected -");
+    }
+})
+
+const rooms = document.querySelectorAll('.room');
+rooms.forEach((room) => {
+    room.addEventListener('mouseup', event => {
+        let diffX = Math.abs(event.pageX - start_x);
+        let diffY = Math.abs(event.pageY - start_y);
+        if (diffX < delta && diffY < delta) {
+            select(event);
+        }
+    })
+    room.addEventListener('keyup', event => {
+        if (event.key == 'Enter') {
+            select(event);
+        }
+    });
+});
+
+
+function deselect() {
+    if (selected) {
+        document.getElementById(selected).classList.remove('selected');
+    }
+}
+
+function select(event) {
+    deselect();
+    selected = event.currentTarget.id;
+    document.getElementById(selected).classList.add('selected');
+    
+    //TODO сделать вывод информации!!!
+    console.log(data[selected]['name']);
+}
+
+/* = Переключение этажа ================================ *\
+\* ===================================================== */
+
+let current_floor = 1;
+
+let min_floor = 1;
+let max_floor = 4;
+
+document.addEventListener('keydown', event => {
+    if (event.key == "ArrowUp") { upFloor(); }
+    if (event.key == "ArrowDown") { downFloor(); }
+})
+
+function upFloor() { 
+    next_floor = current_floor + 1;
+    if (max_floor < next_floor) { return; }
+    changeFloor(next_floor);
+}
+
+function downFloor() {
+    next_floor = current_floor - 1;
+    if (next_floor < min_floor) { return; }
+    changeFloor(next_floor);
+}
+
+function changeFloor(next_floor) {
+    const current = document.getElementById('f'+current_floor);
+    const next = document.getElementById('f'+next_floor);
+
+    if (next_floor > current_floor) {
+        current.classList.add('reverse');
+        next.classList.add('reverse');
+    }
+    else {
+        current.classList.remove('reverse');
+        next.classList.remove('reverse');
+    }
+
+    current.classList.add('hide');
+    next.classList.remove('hide');
+
+    current_floor = next_floor;
+}
